4ea9f58c4d7c13b7686549c4ee864372890075fa
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-30 12:25:57 -0700

    final updated main.h

--- main.h
+++ main.h
@@ -2,12 +2,42 @@
 #define MAIN_H_
 
 #define BUFFER_SIZE 1024
+#define UNUSED(x) (void)(x)
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int _printf(const char *format, ...);
+
+/**
+ * struct FS - Struct for format specifier and associated function
+ *
+ * @sp: The format specifier.
+ * @fn: The associated function.
+ */
+struct FS
+{
+	char sp;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct FS FmtSpec_t - Typedef for FS struct
+ *
+ * @sp: The format specifier.
+ * @fn: The associated function.
+ */
+typedef struct FS FmtSpec_t;
 
 /***Protoypes for Functions used*******/
 int process_Char(char Buff[], int flags, int width, int precision, int size);
@@ -37,8 +67,23 @@ int num_writer(int buffInd, char Buff[],
 	int flags, int width, int precision,
 	int num_length, char filler, char extra);
 void print_buffer(char Buff[], int *buffInd);
+int process_pointer(va_list list, char Buff[],
+		int flags, int width, int precision, int size);
+int process_non_printable(va_list list, char Buff[],
+		int flags, int width, int precision, int size);
+int process_reverse(va_list list, char buffer[],
+		int flags, int width, int precision, int size);
+int process_rot13string(va_list list, char Buff[],
+		int flags, int width, int precision, int size);
+int process_hexadecimal(va_list list, char Buff[],
+	int flags, int width, int precision, int size);
+int processHexaUpper(va_list list, char Buff[],
+		int flags, int width, int precision, int size);
+int process_hexa(va_list list, char map_to[], char Buff[],
+		int flags, char flag_ch, int width, int precision, int size);
+int process_pointer(va_list list, char Buff[],
+		int flags, int width, int precision, int size);
 
-/*****Prototypes for Flags*****/
 int calculate_flags(const char *format, int *index);
 int calculate_width(const char *format, int *index, va_list list);
 int calculatePrecision(const char *format, int *index, va_list list);
