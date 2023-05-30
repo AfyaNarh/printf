5dd091ca559c75407dee734ab24487f37e244cd3
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-30 09:18:35 -0700

    function for unprintable

--- printable_unprintable.c
+++ printable_unprintable.c
@@ -43,3 +43,40 @@ int process_pointer(va_list list, char Buff[],
 	return (writeMemoryAddress(Buff, ind, length,
 				width, flags, filler, extra, filler_start));
 }
+
+/**
+ * process_non_printable - Processes and prints ASCII codes in hexadecimal
+ *                        for non-printable characters
+ * @list: Argument list
+ * @Buff: Buffer array
+ * @flags: Calculates flags
+ * @width: calculate width
+ * @precision: calculate Precision
+ * @size: calculate Size
+ * Return: Number of characters printed.
+ */
+int process_non_printable(va_list list, char Buff[],
+		int flags, int width, int precision, int size)
+{
+	int a = 0, offset_value = 0;
+	char *str = va_arg(list, char *);
+
+	void UNUSED(flags);
+	void UNUSED(width);
+	void UNUSED(precision);
+	void UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+	for (; str[a]; a++)
+	{
+		if (isPrintable(str[a]))
+			Buff[a + offset_value] = str[a];
+		else
+			offset_value += append_hexa_code(str[a], Buff, a + offset_value);
+	}
+	Buff[a + offset_value] = '\0';
+
+	return (write(1, Buff, a + offset_value));
+}
+
