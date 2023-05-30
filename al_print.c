3efd8354965ab4d54cb6011f8d24e8f8d31afea4
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-30 09:50:00 -0700

    updated al_prin.c function names

--- al_print.c
+++ al_print.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_unsigned_binary - Prints an unsigned number in binary format
+ * process_Binary - Prints an unsigned number in binary format
  * @list: List of arguments
- * @Buff: Buffer array to handle print
- * @flags: Active flags
- * @width: Width specification
- * @precision: Precision specification
- * @size:ize specifier
+ * @Buff: Buffer array
+ * @flags: calculates flags
+ * @width: Width calculator
+ * @precision: Precision calculator
+ * @size: size calculator
  * Return: Number of characters printed
  */
-int print_unsigned_binary(va_list list, char Buff[],
+int process_Binary(va_list list, char Buff[],
 	int flags, int width, int precision, int size)
 {
 	unsigned int a, b, c, d, total;
@@ -24,7 +24,7 @@ int print_unsigned_binary(va_list list, char Buff[],
 	(void)size;
 
 	a = va_arg(list, unsigned int);
-	b = 2147483648; /* (2 ^ 31) */
+	b = 2147483648;
 	binary[0] = a / b;
 	for (c = 1; c < 32; c++)
 	{
@@ -48,16 +48,16 @@ int print_unsigned_binary(va_list list, char Buff[],
 
 
 /**
- * print_modified_unsigned - Prints an unsigned number
+ * process_Unsigned - Prints an unsigned number
  * @list: List of arguments
- * @Buff: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: Width specification
- * @precision: Precision specification
- * @size:ize specifier
+ * @Buff: Buffer array
+ * @flags: Calculates flags
+ * @width: Width calculator
+ * @precision: Precision calculator
+ * @size: size calculator
  * Return: Number of characters printed
  */
-int print_modified_unsigned(va_list list, char Buff[],
+int process_Unsigned(va_list list, char Buff[],
 	int flags, int width, int precision, int size)
 {
 	int a = BUFF_SIZE - 2;
@@ -81,16 +81,16 @@ int print_modified_unsigned(va_list list, char Buff[],
 }
 
 /**
- * print_custom_octal - Prints an unsigned number in octal notation
+ * process_Octal - Prints an unsigned number in octal notation
  * @list: List of arguments
- * @Buff: Buffer array to handle print
+ * @Buff: Buffer array
  * @flags: Calculates active flags
- * @width: Get width
- * @precision: Precision specification
- * @size:ize specifier
+ * @width: Calculates width
+ * @precision:Calculates  Precision
+ * @size: Calculates size
  * Return: Number of characters printed
  */
-int print_custom_octal(va_list list, char Buff[],
+int process_Octal(va_list list, char Buff[],
 	int flags, int width, int precision, int size)
 {
 	int a = BUFF_SIZE - 2;
