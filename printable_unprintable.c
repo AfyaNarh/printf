3cc5ee89b87122610248ba6a1fdf5063db66b55d
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-05-30 09:28:29 -0700

    function for reverse printing updated

--- printable_unprintable.c
+++ printable_unprintable.c
@@ -80,3 +80,51 @@ int process_non_printable(va_list list, char Buff[],
 	return (write(1, Buff, a + offset_value));
 }
 
+
+/**
+ * process_reverse - Process and print a string in reverse order.
+ * @list: List of arguments.
+ * @buffer: Buffer array for print handling.
+ * @flags: Active flags for formatting.
+ * @width: Width specification.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ *
+ * Return: Number of characters printed.
+ */
+int process_reverse(va_list list, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	char *str;
+	int total = 0;
+	int length = 0, a;
+
+	void(buffer);
+	void(flags);
+	void(width);
+	void(precision);
+	void(size);
+
+	str = va_arg(list, char *);
+
+	if (str == NULL)
+	{
+		str = "(Null)";
+	}
+
+	/* Calculate the length of the string */
+	while (str[length] != '\0')
+		length++;
+
+	/* Print the string in reverse order */
+	for (a = length - 1; a >= 0; a--)
+	{
+		char ch = str[a];
+
+		write(1, &ch, 1);
+		total++;
+	}
+
+	return (total);
+}
+
